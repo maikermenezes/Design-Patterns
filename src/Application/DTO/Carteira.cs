51214ae24deffc82e4b706cd2eb4d363a36b91c8
Maike Menezes <maikermenezes@gmail.com>
2021-01-14 12:27:33 -0300

    Tentando implementar um novo serviço

---
+++ src/Application/DTO/Carteira.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternSamples.Application.DTO
+{
+    class Carteira
+    {
+    }
+}
---
+++ src/Application/Services/VerificarPontos.cs
@@ -0,0 +1,13 @@
+﻿using DesignPatternSamples.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternSamples.Application.Services
+{
+    class VerificarPontos
+    {
+      public Task<IEnumerable<Carteira>> Verificar(Carteira carteira);
+    }
+}
